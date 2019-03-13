3ba5f535482c97606062a6fee62d845a81a99dff
Maxim <smaximd@gmail.com>
2019-03-13 09:37:51 +0700

    change output and files

--- main.c
+++ main.c
@@ -1,24 +1,27 @@
-#include "mySimpleComputer.h"
+#include "MSC.h"
 
 #include <stdio.h>
 
 int main() {
     sc_regInit();
     sc_memoryInit();
-    sc_memorySet(10, 50);
+    sc_memorySet(10, 45);
     int cell;
     sc_memoryGet(10, &cell);
-    printf("cell - %d\n", cell);
+    printf("\n memory cell value = %d\n", cell);
+    
     sc_memorySave("save.bin");
     sc_memoryLoad("save.bin");
 
     sc_regSet(MEM_OUT, 1);
     int val;
     sc_regGet(1, &val);
-    printf("val - %d\n", val);
-    sc_commandEncode(0x55, 0x59, &val);
+    printf("\n flag value - %d\n", val);
+    
+    sc_commandEncode(0x56, 0x60, &val);
     int command;
     int oper;
     sc_commandDecode(val, &command, &oper);
-    printf("%x %x\n", command, oper);
+    printf("\n command - %x \n", command);
+    printf("\n operand - %x \n", oper);
 }
